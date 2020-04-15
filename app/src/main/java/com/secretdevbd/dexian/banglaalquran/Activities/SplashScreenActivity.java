25b96e13efd9f75a2806c83eb9909b96cf919514
De Xian <ahx.agent007@gmail.com>
2020-04-16 04:08:27 +0600

    api codes

--- app/src/main/java/com/secretdevbd/dexian/banglaalquran/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/secretdevbd/dexian/banglaalquran/Activities/SplashScreenActivity.java
@@ -12,12 +12,16 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.secretdevbd.dexian.banglaalquran.DB.DatabaseHandler;
 import com.secretdevbd.dexian.banglaalquran.DB.ResponseDB;
 import com.secretdevbd.dexian.banglaalquran.R;
+import com.secretdevbd.dexian.banglaalquran.SharedPreffClass;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,6 +40,8 @@ public class SplashScreenActivity extends AppCompatActivity {
         PB_loading = findViewById(R.id.PB_loading);
         PB_loading.setVisibility(View.VISIBLE);
 
+        jsonRequest();
+
 
     }
 
@@ -44,7 +50,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
         final Gson g = new Gson();
 
-        String url = "http://127.0.0.1:5000/API/QURAN"; //http://127.0.0.1:5000/API/QURAN
+        String url = "https://quran.secretdevbd.com/Quran"; //http://127.0.0.1:5000/API/QURAN
         Log.i(TAG, "URL : " + url);
         // Request a string response from the provided URL.
         StringRequest postRequest = new StringRequest(Request.Method.POST, url,
@@ -79,6 +85,7 @@ public class SplashScreenActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         // error
                         Log.i(TAG, "simpleRequestForAllData : "+error);
+                        Log.i(TAG, ""+error.networkResponse.statusCode);
                     }
                 }
         ) {
@@ -86,8 +93,7 @@ public class SplashScreenActivity extends AppCompatActivity {
             protected Map<String, String> getParams()
             {
                 Map<String, String>  params = new HashMap<String, String>();
-                //params.put("data", g.toJson(userInfoJson));
-                //Log.i(TAG, g.toJson(userInfoJson));
+                params.put("version", "1");
 
                 return params;
             }
@@ -97,5 +103,57 @@ public class SplashScreenActivity extends AppCompatActivity {
         // Add the request to the RequestQueue.
         //queue.add(stringRequest);
         queue.add(postRequest);
+    }
+
+    private void jsonRequest(){
+
+        String URL = "https://quran.secretdevbd.com/Quran";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        Map<String, String> jsonParams = new HashMap<String, String>();
+
+        jsonParams.put("version",new SharedPreffClass(getApplicationContext()).getDLstatus());
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                URL,
+                new JSONObject(jsonParams),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String msg = response.toString();
+
+                        Log.i(TAG,"Response : "+msg);
+
+                        Gson g = new Gson();
+                        ResponseDB responseDB = g.fromJson(msg, ResponseDB.class);
+
+                        DatabaseHandler databaseHandler = new DatabaseHandler(getApplicationContext());
+
+                        databaseHandler.addAllArabic(responseDB.getData().getARABIC());
+                        databaseHandler.addAllAudio(responseDB.getData().getAUDIO());
+                        databaseHandler.addAllBangla(responseDB.getData().getBANGLA());
+                        databaseHandler.addAllNames(responseDB.getData().getNAMES());
+                        databaseHandler.addAllPronunciation(responseDB.getData().getPRO());
+
+                        PB_loading.setVisibility(View.INVISIBLE);
+
+                        new SharedPreffClass(getApplicationContext()).setDLstatus("1");
+
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i(TAG,"jsonRequest ERROR :"+error.toString());
+                    }
+                }
+        );
+
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
--- app/src/main/java/com/secretdevbd/dexian/banglaalquran/SharedPreffClass.java
+++ app/src/main/java/com/secretdevbd/dexian/banglaalquran/SharedPreffClass.java
@@ -12,87 +12,24 @@ public class SharedPreffClass {
     }
 
 
-    public void saveIPAddress(String ip){
+    public void setDLstatus(String dl){
         SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
         SharedPreferences.Editor mEditor = mSharedPreferences.edit();
-        mEditor.putString("IP_ADDRESS", ip);
+        mEditor.putString("DL_STATUS", dl);
 
         mEditor.apply();
     }
 
 
-    public String getIPAddress() {
+    public String getDLstatus() {
         SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        String s = mSharedPreferences.getString("IP_ADDRESS", "-1");
+        String s = mSharedPreferences.getString("DL_STATUS", "0");
 
         return s;
     }
 
 
-    public void savePhoneNumber(String P) {
-        SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        SharedPreferences.Editor mEditor = mSharedPreferences.edit();
-        mEditor.putString("PHONE", P);
 
-        mEditor.apply();
-    }
-
-    public String getPhoneNumber() {
-        SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        String s = mSharedPreferences.getString("PHONE", "0");
-
-        return s;
-    }
-
-
-    public void saveSimSerial(String P) {
-        SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        SharedPreferences.Editor mEditor = mSharedPreferences.edit();
-        mEditor.putString("SIM_SERIAL", P);
-
-        mEditor.apply();
-    }
-
-
-    public String getSimSerial() {
-        SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        String s = mSharedPreferences.getString("SIM_SERIAL", "-1");
-
-        return s;
-    }
-
-
-    public void saveUserName(String P) {
-        SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        SharedPreferences.Editor mEditor = mSharedPreferences.edit();
-        mEditor.putString("USER_NAME", P);
-
-        mEditor.apply();
-    }
-
-
-    public String getUserName() {
-        SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        String s = mSharedPreferences.getString("USER_NAME", "Null");
-
-        return s;
-    }
-
-    public void saveUserPic(String P) {
-        SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        SharedPreferences.Editor mEditor = mSharedPreferences.edit();
-        mEditor.putString("USER_PIC", P);
-
-        mEditor.apply();
-    }
-
-
-    public String getUserPic() {
-        SharedPreferences mSharedPreferences = context.getSharedPreferences("DATA", Context.MODE_PRIVATE);
-        String s = mSharedPreferences.getString("USER_PIC", "Null");
-
-        return s;
-    }
 
 
 
